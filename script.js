71db5f2ad313228d398b02d68021ce1e76c6ed28
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-07 23:08:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let cityInput = document.getElementById('city_input'),
 searchBtn = document.getElementById('searchBtn'),
 api_key = '51277cfa222ea09427f35318ca1969e0',
 currentWeatherCard = document.querySelectorAll('.weather-left .card')[0],
-fiveDaysForecastCard = document.querySelector('.day-forecast');
+fiveDaysForecastCard = document.querySelector('.day-forecast'),
+aqiCard = document.querySelectorAll('.highlights .card')[0];
 
 function getWeatherDetails(name, lat, lon, country, state) {
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
     WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    AIR_POLLUTION_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    
+
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
