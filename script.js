bcd3ce6cc13c69beb260926443a0241d993ba2cc
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-10 21:14:55 +0700

    API Integration in Right Section
    Give API into right section and fix bug in the browser

--- script.js
+++ script.js
@@ -1,14 +1,84 @@
 let cityInput = document.getElementById('city_input'),
 searchBtn = document.getElementById('searchBtn'),
-api_key = 'ef54adefd889b2cfb1fa8e73ee856710',
+locationBtn = document.getElementById('locationBtn'),
+api_key = '51277cfa222ea09427f35318ca1969e0',
 currentWeatherCard = document.querySelectorAll('.weather-left .card')[0],
-fiveDaysForecastCard = document.querySelector('.day-forecast');
+fiveDaysForecastCard = document.querySelector('.day-forecast'),
+aqiCard = document.querySelectorAll('.highlights .card')[0],
+sunriseCard = document.querySelectorAll('.highlights .card')[1],
+humidityVal = document.getElementById('humidityVal'),
+pressureVal = document.getElementById('pressureVal'),
+visibilityVal = document.getElementById('visibilityVal'),
+windSpeedVal = document.getElementById('windSpeedVal'),
+feelsVal = document.getElementById('feelsVal'),
+hourlyForecastCard = document.querySelector('.hourly-forecast'),
+aqiList = ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor'];
 
 function getWeatherDetails(name, lat, lon, country, state) {
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
     WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    AIR_POLLUTION_API_URL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`,
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+    fetch(AIR_POLLUTION_API_URL).then(res => res.json()).then(data => {
+        // console.log(data);   // checking the data
+        let {co, no, no2, o3, so2, pm2_5, pm10, nh3} = data.list[0].components;
+        aqiCard.innerHTML = `
+            <div class="card-head">
+                <p>Air Quality Index</p>
+                <p class="air-index aqi-${data.list[0].main.aqi}">${aqiList[data.list[0].main.aqi - 1]}</p>
+            </div>
+
+            <div class="air-indices">
+                <i class="fa-regular fa-wind fa-3x"></i>
+                
+                <div class="item">
+                    <p>PM2.5</p>
+                    <h2>${pm2_5}</h2>
+                </div>
+
+                <div class="item">
+                    <p>PM10</p>
+                    <h2>${pm10}</h2>
+                </div>
+
+                <div class="item">
+                    <p>SO2</p>
+                    <h2>${so2}</h2>
+                </div>
+
+                <div class="item">
+                    <p>CO</p>
+                    <h2>${co}</h2>
+                </div>
+
+                <div class="item">
+                    <p>NO</p>
+                    <h2>${no}</h2>
+                </div>
+                
+                <div class="item">
+                    <p>NO2</p>
+                    <h2>${no2}</h2>
+                </div>
+
+                <div class="item">
+                    <p>NH3</p>
+                    <h2>${nh3}</h2>
+                </div>
+
+                <div class="item">
+                    <p>O3</p>
+                    <h2>${o3}</h2>
+                </div>
+
+            </div>   
+        `;
+    }).catch(() => {
+        alert('Failed to fetch air pollution data');
+    });
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
         // console.log(data);   // checking the data
@@ -32,12 +102,76 @@ function getWeatherDetails(name, lat, lon, country, state) {
                 <p><i class="fa-light fa-location-dot"></i> ${name}, ${country}</p>
             </div>
         `;
+        let {sunrise, sunset} = data.sys,
+        {timezone, visibility} = data,
+        {humidity, pressure, feels_like} = data.main,
+        {speed} = data.wind,
+        sRiseTime = moment.utc(sunrise, 'x').add(timezone, 'seconds').format('hh:mm A'),
+        sSetTime = moment.utc(sunset, 'x').add(timezone, 'seconds').format('hh:mm A');
+        sunriseCard.innerHTML = `
+            <div class="card-head">
+                <p>Sunrise & Sunset</p>
+            </div>
+
+            <div class="sunrise-sunset">
+                
+                <div class="item">
+                    <div class="icon">
+                        <i class="fa-light fa-sunrise fa-4x"></i>
+                    </div>
+
+                    <div>
+                        <p>Sunrise</p>
+                        <h2>${sRiseTime}</h2>
+                    </div>
+
+                </div>
+
+                <div class="item">
+                    <div class="icon">
+                        <i class="fa-light fa-sunset fa-4x"></i>
+                    </div>
+
+                    <div>
+                        <p>Sunset</p>
+                        <h2>${sSetTime}</h2>
+                    </div>
+
+                </div>
+
+            </div>
+        `;
+        humidityVal.innerHTML = `${humidity}%`;
+        pressureVal.innerHTML = `${pressure}hPa`;
+        visibilityVal.innerHTML = `${visibility / 1000}km`;
+        windSpeedVal.innerHTML = `${speed}m/s`;
+        feelsVal.innerHTML = `${(feels_like - 273.15).toFixed(2)}&deg;C`;
     }).catch(() => {
         alert('Failed to fetch current weather');
     });
 
     fetch(FORECAST_API_URL).then(res => res.json()).then(data => {
         // console.log(data);   // checking the data
+        let hourlyForecast = data.list;
+        hourlyForecastCard.innerHTML = '';
+        for(i = 0; i <= 7; i++) {
+            let hrForecastDate = new Date(hourlyForecast[i].dt_txt);
+            let hr = hrForecastDate.getHours();
+            let a = 'PM';
+            if(hr < 12)
+                a = 'AM';
+            if(hr == 0)
+                hr = 12;
+            if(hr > 12)
+                hr -= 12;
+            hourlyForecastCard.innerHTML += `
+                <div class="card">
+                    <p>${hr} ${a}</p>
+                    <img src="https://openweathermap.org/img/wn/${hourlyForecast[i].weather[0].icon}.png" alt="">
+                    <p>${(hourlyForecast[i].main.temp -273.15).toFixed(2)}&deg;C</p>
+                </div>
+            `;
+        }
         let uniqueForecastDays = [];
         let fiveDaysForecast = data.list.filter(forecast => {
             let forecastDate = new Date(forecast.dt_txt).getDate();
@@ -81,4 +215,27 @@ function getCityCoordinates() {
     });
 }
 
+function getUserCoordinates() {
+    navigator.geolocation.getCurrentPosition(position => {
+        let {latitude, longitude} = position.coords;
+        // console.log(latitude, longitude);
+        let REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${api_key}`;
+        
+        fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data => {
+            // console.log(data);   // checking the data
+            let {name, country, state} = data[0];
+            getWeatherDetails(name, latitude, longitude, country, state);
+        }).catch(() => {
+            alert('Failed to fetch location data');
+        });
+    
+    }, error => {
+        if(error.code === error.PERMISSION_DENIED)
+            alert('Geolocation permission denied. Please reset location permission to grant access again');
+    });
+}
+
 searchBtn.addEventListener('click', getCityCoordinates);
+locationBtn.addEventListener('click', getUserCoordinates);
+cityInput.addEventListener('keyup', e => e.key === 'Enter' && getCityCoordinates());
+window.addEventListener('load', getUserCoordinates);
